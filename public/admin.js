61b0292bd02d56383af63e0603b3c15a9224f90b
Vanessa Kasminoff <sunnset07@gmail.com>
2023-12-28 20:29:56 -0800

    Added functionality to the admin portal allowing user to input the quantities of books and save it, reflecting on the main page.

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,48 @@
+//Retrieves the list of books and executes renderBookList function for each book.
+async function retrieveBooks() {
+    let response = await fetch("http://localhost:3001/listBooks");
+    let books = await response.json();
 
-// Your Code Here
+    books.forEach(renderBookList);
+};
+
+//Targets the html div with id#root and creates a list item for each book.
+//Also creates two inputs: one input for quanity of books, another to submit.
+function renderBookList(book) {
+    let div = document.getElementById("root");
+
+    let li = document.createElement("li");
+    li.textContent = book.title;
+
+    let input = document.createElement("input");
+    input.type = "number";
+    input.value = book.quantity;
+
+    let submit = document.createElement("input");
+    submit.type = "submit";
+    submit.value = "Save";
+
+    //Updates the book list with the input quantity when the submit button is clicked.
+    submit.addEventListener('click', async () => {
+        let response = await fetch("http://localhost:3001/updateBook", {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: input.value,
+            }),
+        });
+
+        let updatedBookQuantity = await response.json();
+        console.log(updatedBookQuantity);
+    });
+
+    //Inserts the list items into the div.
+    div.append(li);
+    //Inserts the input fields and submit buttons next to the list items.
+    li.append(input, submit);
+};
+
+retrieveBooks();
--- public/index.js
+++ public/index.js
@@ -1,28 +1,53 @@
+//Retrieves the list of books and executes renderBook function for each book.
 async function main() {
+  let response = await fetch("http://localhost:3001/listBooks");
+  let books = await response.json();
 
-    let response = await fetch('http://localhost:3001/listBooks')
-    let books = await response.json()
-
-    books.forEach(renderBook)
+  books.forEach(renderBook);
 }
 
+//Creates the html cards for the books.
 function renderBook(book) {
-    let bookContainer = document.querySelector('.book-container')
-    bookContainer.innerHTML += `
+  let bookContainer = document.querySelector(".book-container");
+  bookContainer.innerHTML += `
         <div class="col-sm-3">
             <div class="card" style="width: 100%;">
-                ${book.imageURL ? `
+                ${
+                  book.imageURL
+                    ? `
                     <img class="card-img-top" src="${book.imageURL}" />
                 `
-                : ``}
+                    : ``
+                }
                 <div class="card-body">
                     <h5 class="card-title">${book.title}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Available: ${book.quantity}</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">Available: ${
+                      book.quantity
+                    }</h6>
                     <p class="card-text">${book.description}</p>
                 </div>
             </div>
         </div>
-    `
+    `;
 }
 
-main()
+//To update the third book's title.
+async function updateBookFunc() {
+  let response = await fetch("http://localhost:3001/updateBook", {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: 3,
+      title: "Legends of Arathrae",
+    }),
+  });
+
+  let updatedBook = await response.json();
+  console.log(updatedBook);
+}
+
+updateBookFunc();
+
+main();
